ef1fef2c3b60e93b5f36101c54cb347833bbb250
gabrielsilvi <gabrielsv3000@hotmail.com>
2024-02-16 03:13:04 -0300

    first commit

---
+++ cars/forms.py
@@ -0,0 +1,40 @@
+from django import forms
+from cars.models import Car
+
+
+class CarModelForm(forms.ModelForm):
+    
+    class Meta:
+        model = Car
+        fields = '__all__'
+        
+    def clean_value(self):
+        value = self.cleaned_data.get('value')
+        if value <1000:
+            self.add_error('value', 'Valor tem que ser maior que R$1.000')
+        return value
+    
+# *** maneira mais complicada ***
+# class CarForm(forms.Form):
+#     model = forms.CharField(max_length=200)
+#     brand = forms.ModelChoiceField(Brand.objects.all())
+#     factory_year = forms.IntegerField()
+#     model_year = forms.IntegerField()
+#     plate = forms.CharField(max_length=10, )
+#     value = forms.FloatField()
+#     photo = forms.ImageField()
+
+#     def save(self):
+#         car = Car(
+#             model = self.cleaned_data['model'],
+#             brand = self.cleaned_data['brand'],
+#             factory_year = self.cleaned_data['factory_year'],
+#             model_year = self.cleaned_data['model_year'],
+#             plate = self.cleaned_data['plate'],
+#             value = self.cleaned_data['value'],
+#             photo = self.cleaned_data['photo'],
+#         )
+#         car.save()
+#         return car
+
+# *** maneira mais facil usando modelForms***       
